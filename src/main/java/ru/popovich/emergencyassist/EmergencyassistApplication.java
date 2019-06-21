bedc5dd9437f746c7cf9608716f9cbe04057575d
gorynychalex <gorynychalex@gmail.com>
2019-06-22 08:44:42 +1000

    deploy to war added

--- src/main/java/ru/popovich/emergencyassist/EmergencyassistApplication.java
+++ src/main/java/ru/popovich/emergencyassist/EmergencyassistApplication.java
@@ -2,12 +2,18 @@ package ru.popovich.emergencyassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmergencyassistApplication {
+public class EmergencyassistApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmergencyassistApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmergencyassistApplication.class);
+	}
 }
