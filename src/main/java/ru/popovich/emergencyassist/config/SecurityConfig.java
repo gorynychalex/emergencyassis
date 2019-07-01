907573e19afca673b1accfb969d19fd9abb86b6b
gorynychalex <gorynychalex@gmail.com>
2019-07-01 10:05:16 +1000

    ActiveDirectory auth added

--- src/main/java/ru/popovich/emergencyassist/config/SecurityConfig.java
+++ src/main/java/ru/popovich/emergencyassist/config/SecurityConfig.java
@@ -1,30 +1,69 @@
 package ru.popovich.emergencyassist.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
-import ru.popovich.emergencyassist.model.User;
-import ru.popovich.emergencyassist.repository.UserDao;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${activedirectory.domain}")
+    private String AD_DOMAIN;
+
+    @Value("${activedirectory.url}")
+    private String AD_URL;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
-                .mvcMatchers("/","/login**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .csrf().disable();
+                .httpBasic()
+        ;
+//        http
+//                .authorizeRequests()
+//                .mvcMatchers("/","/login**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .authenticationProvider(activeDirectoryAuthenticationProvider());
+    }
+
+    /**
+     *
+     * https://medium.com/@dmarko484/spring-boot-active-directory-authentication-5ea04969f220
+     * (https://medium.com/@viraj.rajaguru/how-to-use-spring-security-to-authenticate-with-microsoft-active-directory-1caff11c57f2)
+     *
+     * @return
+     */
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(Arrays.asList(activeDirectoryAuthenticationProvider()));
+    }
+
+    public AuthenticationProvider activeDirectoryAuthenticationProvider(){
+        ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider(AD_DOMAIN, AD_URL);
+        provider.setConvertSubErrorCodesToExceptions(true);
+        provider.setUseAuthenticationRequestCredentials(true);
+        return provider;
     }
 
     /**
@@ -33,39 +72,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      * @param auth
      * @throws Exception
      */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userDnPatterns("cn={0},ou=ОФФИС,dc=pcson,dc=ru")
-                .groupSearchBase("dc=pcson,dc=ru")
-                .contextSource()
-                .url("ldap://srv-dc.pcson.ru")
-                .and()
-                .passwordCompare()
-                .passwordEncoder(new LdapShaPasswordEncoder())
-                .passwordAttribute("samaccountname");
-    }
-
-    @Bean
-    public PrincipalExtractor principalExtractor(UserDao userDao){
-        return map -> {
-            String name = (String) map.get("name");
-            User user1 = userDao.findById(name).orElseGet(()->{
-
-                User newUser = new User();
-
-                newUser.setSub((String)map.get("sub"));
-                newUser.setNickname((String)map.get("name"));
-                newUser.setEmail((String)map.get("email"));
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .ldapAuthentication()
+//                .userDnPatterns("cn={0},ou=ОФФИС,dc=pcson,dc=ru")
+//                .groupSearchBase("dc=pcson,dc=ru")
+//                .contextSource()
+//                .url("ldap://srv-dc.pcson.ru")
+//                .and()
+//                .passwordCompare()
+//                .passwordEncoder(new LdapShaPasswordEncoder())
+//                .passwordAttribute("samaccountname");
+//    }
 
 
-                return newUser;
-            });
 
-            userDao.save(user1);
-            return user1;
-        };
-
-    }
+//    @Bean
+//    public PrincipalExtractor principalExtractor(UserDao userDao){
+//        return map -> {
+//            String name = (String) map.get("name");
+//            User user1 = userDao.findById(name).orElseGet(()->{
+//
+//                User newUser = new User();
+//
+//                newUser.setSub((String)map.get("sub"));
+//                newUser.setNickname((String)map.get("name"));
+//                newUser.setEmail((String)map.get("email"));
+//
+//
+//                return newUser;
+//            });
+//
+//            userDao.save(user1);
+//            return user1;
+//        };
+//
+//    }
 }
