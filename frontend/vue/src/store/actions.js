cce3376e250633ab36501fa05b129496c13ae007
gorynychalex <gorynychalex@gmail.com>
2019-08-13 09:32:20 +1000

    refactor simple views: services, users, tasks

--- frontend/vue/src/store/actions.js
+++ frontend/vue/src/store/actions.js
@@ -1,24 +1,27 @@
+import { FETCH_SERVICES, FETCH_USERS, FETCH_TASKS } from "./actions/services";
+import { service, user, task } from "./actions/services";
+
 export default {
-    FETCH_SERVICES: (context, species) => {
-        fetch('/api/v1/service')
+    [FETCH_SERVICES]: ({ commit, dispatch }, species) => {
+        fetch('/api/v1/' + service)
             .then(r => r.json())
             .then(r => {
-                context.commit("FETCH_SERVICES", r)
+                commit(FETCH_SERVICES, r)
             })
     },
-    FETCH_USERS: ({ commit, dispatch }, species) => {
+    [FETCH_USERS]: ({ commit, dispatch }, species) => {
         console.log(dispatch)
-        fetch('/api/v1/user')
+        fetch('/api/v1/' + user)
             .then(r => r.json())
             .then(r => {
-                commit("FETCH_USERS", r)
+                commit(FETCH_USERS, r)
             })
     },
-    FETCH_TASKS: ({ commit, dispatch }, species) => {
-        fetch('/api/v1/task')
+    [FETCH_TASKS]: ({ commit, dispatch }, species) => {
+        fetch('/api/v1/' + task)
             .then(r => r.json())
             .then(r => {
-                commit("FETCH_TASKS", r)
+                commit(FETCH_TASKS, r)
             })
     }
 }
