5feed0d39bd7b542f4f094367c00ec49ff89039f
gorynychalex <gorynychalex@gmail.com>
2019-09-23 16:41:22 +1000

    UserRoles

--- src/main/java/ru/popovich/emergencyassist/model/UserRole.java
+++ src/main/java/ru/popovich/emergencyassist/model/UserRole.java
@@ -1,7 +1,20 @@
 package ru.popovich.emergencyassist.model;
 
 public enum UserRole {
+
     HARDUP,
+
     EMPLOYEE,
+
+    SPECIALIST,
+
+    ANALYST,
+
+    CHIEF,
+
+    DIRECTOR,
+
+    ACCOUNTANT,
+
     ADMIN
 }
